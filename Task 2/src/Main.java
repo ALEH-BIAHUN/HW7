3ef02e5949fe30ae94e832f6358790b5aa76e5fc
Aleh Biahun <stones2875@gmail.com>
2024-05-21 10:29:01 +0200

    The solution to task number 7

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+
+        for(int i = 2; i <=100; i = i + 2) {
+            if(i % 2 == 0) {
+                System.out.println("Уволен айтишник с ID " + i);
+            }
+        }
     }
 }
